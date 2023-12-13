5d7796ba3d4feda1e4eb004c9ae4904e59fc9c17
Jack <kyle-j5@ulster.ac.uk>
2023-12-13 13:22:25 +0000

    Updated

--- app/src/main/java/com/example/ecocarbontracker/CarbonFootprintCalculator.java
+++ app/src/main/java/com/example/ecocarbontracker/CarbonFootprintCalculator.java
@@ -2,22 +2,22 @@ package com.example.ecocarbontracker;
 
 public class CarbonFootprintCalculator {
 
-    // Constants for carbon emission factors (realistic values)
+    // Estimates for carbon emission factors
     private static final double CAR_MILEAGE_EMISSION_FACTOR = 0.2; // in kg CO2 per mile
     private static final double ELECTRICITY_EMISSION_FACTOR = 0.4; // in kg CO2 per kWh
     private static final double MEAT_EMISSION_FACTOR = 27.0; // in kg CO2 per kg of meat
 
-    // Calculate carbon footprint for transportation
+    // Transportation
     public static double calculateTransportationFootprint(double milesDriven) {
         return milesDriven * CAR_MILEAGE_EMISSION_FACTOR;
     }
 
-    // Calculate carbon footprint for home energy usage
+    // Home energy usage
     public static double calculateEnergyFootprint(double kWhUsed) {
         return kWhUsed * ELECTRICITY_EMISSION_FACTOR;
     }
 
-    // Calculate carbon footprint for diet
+    // Diet
     public static double calculateDietFootprint(double meatConsumed) {
         // Convert meat consumption from grams to kilograms
         double meatInKg = meatConsumed/1000;
